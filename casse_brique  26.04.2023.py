06ab9846c0c953b04c5092f9ddf2e6d88b37c11e
DROUET TAOS <TAOS.DRT@ge-pedago.etat-ge.ch>
2023-05-03 11:11:07 +0200

    version départ

---
+++ casse_brique  26.04.2023.py
@@ -0,0 +1,27 @@
+import turtle as tr
+
+window = tr.Screen()
+window.bgcolor("black")
+window.setup(width=600, height=600)
+
+
+barre = tr.Turtle()
+barre.speed(0)
+barre.shape("square")
+barre.shapesize(stretch_wid = 1, stretch_len=5)
+barre.color("red")
+barre.penup()
+barre.setposition(0,-225)
+
+def aller_droite():
+    print("test")
+    barre.forward(30)
+    
+def aller_gauche():
+    barre.backward(30)
+ 
+ 
+ 
+tr.onkey(aller_droite,"d")
+tr.onkey(aller_gauche,"a")
+window.listen()
